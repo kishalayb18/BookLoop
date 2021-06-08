70bc6ed34b5b0abaf70b68a4a11c9367ec4ccb1a
KISHALAY BHATTACHARYA <bhattacharyakishalay@gmail.com>
2021-06-09 03:21:11 +0530

    Almost completed the backend of SignUp, couldn't test it due to some existing errors in other files
    Edited by Kishalay

--- app/src/main/java/com/mypackage/bookloop/SignupActivity.java
+++ app/src/main/java/com/mypackage/bookloop/SignupActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -42,12 +48,52 @@ public class SignupActivity extends AppCompatActivity {
 
         btnSignup=findViewById(R.id.btn_signup);
 
-        private void addUser(String email, String password)
-        {
-            mAuth=FirebaseAuth.getInstance();
+        btnSignup.setOnClickListener(v -> {
+            String name=inpName.getText().toString();
+            String email=inpEmail.getText().toString().trim();
+            String phn=inpPhn.getText().toString();
+            String pass=inpPass.getText().toString().trim();
 
-            mAuth.createUserWithEmailAndPassword(email,password)
-                    .addOnCompleteListener()
-        }
+            addUser(email, pass, name, phn);
+        });
+
+
+    }
+
+    private void addUser(String email, String password, String name,String phn){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        Toast.makeText(this, "Success! Verification email sent.",Toast.LENGTH_SHORT).show();
+                        FirebaseUser user=mAuth.getCurrentUser();
+                        String uid=user.getUid();
+                        user.sendEmailVerification();
+                        addProfile(uid, name, email, phn, password);
+                    }else{
+                        Toast.makeText(this, "Failed.",Toast.LENGTH_SHORT).show();
+                        mAuth.getCurrentUser().delete();
+                    }
+                }).addOnFailureListener(e -> {
+            Toast.makeText(this, e.getMessage(),Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void addProfile(String uid, String name, String email, String phn, String password){
+        reference= FirebaseDatabase.getInstance().getReference("BookLoopUserAccount");
+        Map<String , String> userJson=new HashMap<>();
+        userJson.put("u_name", name);
+        userJson.put("u_email", email);
+        userJson.put("u_age", phn);
+        userJson.put("u_pwd", password);
+
+        reference.child(uid).setValue(userJson).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                Toast.makeText(this, "Success! Verification email sent.",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "User exists",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(e -> {
+            Toast.makeText(this, e.getMessage(),Toast.LENGTH_SHORT).show();
+        });
     }
 }
