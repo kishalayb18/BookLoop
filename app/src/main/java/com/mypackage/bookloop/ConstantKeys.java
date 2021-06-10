c7ab242dc17f23432e59e967daf686a9cbfb868e
KISHALAY BHATTACHARYA <bhattacharyakishalay@gmail.com>
2021-06-11 02:21:33 +0530

    Added ConstantKeys Deleled BLUser
    BLUser had no use

---
+++ app/src/main/java/com/mypackage/bookloop/ConstantKeys.java
@@ -0,0 +1,12 @@
+package com.mypackage.bookloop;
+
+public class ConstantKeys
+{
+    public static String KEY_NAME="u_name";
+    public static final String KEY_EMAIL="u_email";
+    public static String KEY_PHONE="u_phone";
+    public static final String KEY_UID="uid";
+    public static final String KEY_IS_LOGGED="logged_in";
+    public static String KEY_CONTACT_LIST="contactList";
+    //public static String KEY_IMAGE="image";
+}
