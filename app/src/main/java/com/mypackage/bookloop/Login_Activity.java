dab0948fbc6be8ac7b88a99e7b431ca244301b31
SWAPNIL MISHRA <swapnilmishra2808@gmail.com>
2021-06-10 22:33:44 +0530

    Mapped variables of Login_Activity.java

--- app/src/main/java/com/mypackage/bookloop/Login_Activity.java
+++ app/src/main/java/com/mypackage/bookloop/Login_Activity.java
@@ -4,17 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
 
 public class Login_Activity extends AppCompatActivity {
     
     TextView signUp;
+    TextInputLayout layEmail, layPwd;
+    TextInputEditText inpEmail,inpPass;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        layEmail=findViewById(R.id.layout_email);
+        layPwd=findViewById(R.id.edit_password);
+        inpEmail=findViewById(R.id.user_mail);
+        inpPass=findViewById(R.id.user_password);
+        btnLogin=findViewById(R.id.login_btn);
         signUp = findViewById(R.id.Signup);
         signUp.setOnClickListener(v -> {
             Intent r = new Intent(Login_Activity.this, SignUpActivity.class);
