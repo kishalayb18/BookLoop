37fcfee06afa9805b4230dcc18a92cdb48b164fc
KISHALAY BHATTACHARYA <bhattacharyakishalay@gmail.com>
2021-06-22 00:51:46 +0530

    MyUploads Second Modifications
    Edit and Delete icons are clickable now, need to implement the logics.

--- app/src/main/java/com/mypackage/bookloop/MyUploadsAdapter.java
+++ app/src/main/java/com/mypackage/bookloop/MyUploadsAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -40,6 +41,32 @@ public class MyUploadsAdapter extends RecyclerView.Adapter<MyUploadsAdapter.MyUp
         BookListModel bookListModel= bookListFull.get(position);
         holder.bookName.setText("Book Name: "+bookListModel.getBookName());
 
+
+        holder.editMyUploads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(mContext,"Edit clicked",Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(mContext, MyProfileActivity.class);
+                mContext.startActivity(intent);
+                /**
+                 * CODE TO EDIT SHOULD BE ADDED
+                 */
+            }
+        });
+
+
+        holder.deleteMyUploads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(mContext,"Delete clicked",Toast.LENGTH_SHORT).show();
+                /**
+                 * CODE TO DELETE SHOULD BE ADDED
+                 */
+            }
+        });
+
 //        holder.itemView.setOnClickListener(v -> {
 //            Intent intent=new Intent(mContext, BookDetails.class);
 //            intent.putExtra(ConstantKeys.KEY_AUTHOR_NAME, bookListModel.getAuthorName());
@@ -63,14 +90,16 @@ public class MyUploadsAdapter extends RecyclerView.Adapter<MyUploadsAdapter.MyUp
     public class MyUploadsHolder extends RecyclerView.ViewHolder {
 
         private CardView cardView;
-        private TextView bookName;
+        private TextView bookName, editMyUploads, deleteMyUploads;
 
 
         //NEED TO ADD DELETE AND EDIT ICON
         public MyUploadsHolder(@NonNull View itemView) {
             super(itemView);
-            cardView=itemView.findViewById(R.id.ui_card);
-            bookName=itemView.findViewById(R.id.ui_book_name);
+            cardView=itemView.findViewById(R.id.ui_card_my_uploads);
+            bookName=itemView.findViewById(R.id.ui_book_name_mu);
+            editMyUploads=itemView.findViewById(R.id.edit_myuploads);
+            deleteMyUploads=itemView.findViewById(R.id.delete_myuploads);
         }
     }
 
